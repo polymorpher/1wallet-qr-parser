c30a28b2fc87d29b07156ed7f82d3866e4785649
Aaron Li <aaron@qokka.ai>
2022-01-11 10:58:01 -0800

    write secret to separate folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const { MigrationPayload } = require('../proto/oauthMigration')
 const IN_DIR = process.env.IN_DIR || 'inputs'
 const OUT_DIR = process.env.OUT_DIR || 'outputs'
+const OUT_TEXT_DIR = process.env.OUT_TEXT_DIR || 'outputs-text'
 const fs = require('fs').promises
 const qrcode = require('qrcode')
 const jsqr = require('jsqr')
 const path = require('path')
 const sharp = require('sharp')
 const crypto = require('crypto')
+const b32 = require('hi-base32')
 async function main () {
   await fs.mkdir(IN_DIR, { recursive: true })
   await fs.mkdir(OUT_DIR, { recursive: true })
+  await fs.mkdir(OUT_TEXT_DIR, { recursive: true })
   const files = await fs.readdir(IN_DIR)
   for (const file of files) {
     const p = path.join(IN_DIR, file)
@@ -43,7 +46,9 @@ async function main () {
       .map(e => e.replace(/ /g, '-').toLowerCase())
     for (const [ind, param] of filteredParams.entries()) {
       const oFilename = outFilenames[ind] + '-' + crypto.createHash('sha256').update(param.secret).digest('hex') + '.png'
+      const otFilename = outFilenames[ind] + '-' + crypto.createHash('sha256').update(param.secret).digest('hex') + '.txt'
       const op = path.join(OUT_DIR, oFilename)
+      const otp = path.join(OUT_TEXT_DIR, otFilename)
       const payload = MigrationPayload.create({
         otpParameters: [{
           issuer: 'Harmony',
@@ -61,10 +66,12 @@ async function main () {
       const b64 = Buffer.from(bytes).toString('base64')
       const uri = `otpauth-migration://offline?data=${encodeURIComponent(b64)}`
       try {
+        await fs.writeFile(otp, JSON.stringify({ issuer: param.issuer, name: param.name, secret: b32.encode(param.secret) }), { encoding: 'utf-8' })
         const dataUrl = await qrcode.toDataURL(uri, { errorCorrectionLevel: 'low', width: 256 })
         const b = Buffer.from(dataUrl.split(',')[1], 'base64')
         await sharp(b).png().toFile(op)
-        console.log('Wrote to', op)
+        console.log('Wrote QR code to', op)
+        console.log('Wrote plaintext (with b32 encoded secret) to', op)
       } catch (ex) {
         console.error('Failed to write QR code for', op)
         console.error(ex)
